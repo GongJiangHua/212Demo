1f7ddd50503c0ed565b3daa5cd167f82af2ad538
ZJ-p007 <zj636500@qq.com>
2020-12-26 13:41:25 +0800

    22

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("龚江华到此一游")
-<<<<<<< HEAD
+	<<<<<<< HEAD
 	fmt.Println("ZJ")
 	fmt.Println("龚江华到处二游")
-=======
+	====== =
 	fmt.Println("pq到此一游")
->>>>>>> remotes/origin/pq
+	>>>>>>> remotes / origin / pq
 
-fmt.Println("zj到此一游")
+	fmt.Println("zj到此一游")
 }
