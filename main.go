61acca14dcd4b0e3b07f065be39872d382a33315
pq124 <1084146423@qq.com>
2020-12-26 13:42:32 +0800

    json

--- main.go
+++ main.go
@@ -6,12 +6,5 @@ func main() {
 	fmt.Println("龚江华到此一游")
 	fmt.Println("ZJ")
 	fmt.Println("龚江华到处二游")
-	fmt.Println("pq到此一游")
-<<<<<<< HEAD
-	fmt.Println("pq到此一游")
-	fmt.Println("pq到此一游")
 
-=======
-	fmt.Println("龚江华到此三游")
->>>>>>> origin
 }
