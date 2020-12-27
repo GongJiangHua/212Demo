07e6d7b5d1ced02c0261803403fc0c168c461eeb
GongJiangHua <1914805653@qq.com>
2020-12-27 20:17:12 +0800

    1227 gjh second

--- main.go
+++ main.go
@@ -4,31 +4,19 @@ import "fmt"
 
 func main() {
 	fmt.Println("龚江华到此一游")
-	
 	fmt.Println("ZJ")
 	fmt.Println("龚江华到处二游")
-
 	fmt.Println("pq到此一游")
-
 	fmt.Println("zj到此一游")
-
 	fmt.Println("龚江华到此三游")
-<<<<<<< HEAD
-
 	fmt.Println("ZJ到此一游")
-
-=======
 	fmt.Println("pq到此一游")
 	fmt.Println("pq到此一游")
 	fmt.Println("龚江华到处四游")
 	fmt.Println("龚江华到处四游")
 	fmt.Println("=========================")
 	fmt.Println("=========================")
-<<<<<<< HEAD
 	fmt.Println("zj到此二游")
->>>>>>> master
-=======
 	fmt.Println("gjh到此一游")
-
->>>>>>> remotes/origin/pq
+	fmt.Println("=========================")
 }
